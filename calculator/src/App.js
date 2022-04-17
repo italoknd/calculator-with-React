74c4b99daa2578d49699be48cb67f939ebd6af77
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-04-17 03:24:41 -0400

    projeto calculadora criado

---
+++ calculator/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calculatora</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
