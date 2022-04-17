88203bc3cd2a184069876e6089193e2b4583b404
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-04-17 06:37:05 -0400

    calculadora quase finalizada

--- calculator/src/components/Calculator.jsx
+++ calculator/src/components/Calculator.jsx
@@ -1,31 +1,125 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from "react";
+import React, { useState } from 'react'
 import './Calculator.css'
 
 export default props => {
-  return(
+  const initialState = {
+    initialvalue: 0
+  }
+
+  const [num, setNum] = useState(0)
+  const [oldNum, setOldNum] = useState(0)
+  const [operator, setOperator] = useState(0)
+
+  function addDigit(value) {
+    if (num === 0) {
+      setNum(value.target.value)
+    } else {
+      setNum(num + value.target.value)
+    }
+
+    if (num === ',' && setNum.includes(',')) {
+      return
+    }
+
+    if (num > 0) {
+      clearMemory()
+      // setNum(num)
+    }
+  }
+
+  function clearMemory() {
+    setNum(initialState.initialvalue)
+  }
+
+  function percentage() {
+    setNum((num / 100).toString().replace('.', ','))
+  }
+
+  function changeSign() {
+    if (num > 0) {
+      setNum(-num)
+    } else {
+      setNum(Math.abs(num))
+    }
+  }
+
+  function operatorHandler(e) {
+    let operatorValue = e.target.value
+    setOperator(operatorValue)
+    setOldNum(num)
+    setNum(0)
+  }
+
+  function showResult() {
+    if (operator === '+') {
+      setNum(parseFloat(oldNum) + parseFloat(num))
+    } else if (operator === '-') {
+      setNum(parseFloat(oldNum) - parseFloat(num))
+    } else if (operator === 'x') {
+      setNum(parseFloat(oldNum) * parseFloat(num))
+    } else if (operator === '/') {
+      setNum(
+        (parseFloat(oldNum) / parseFloat(num)).toString().replace('.', ',')
+      )
+    }
+  }
+
+  return (
     <div className="calculator">
-      <h1 className="result">0</h1>
+      <h1 className="result">{num}</h1>
       <div className="commands">
-        <button>AC</button>
-        <button>+/-</button>
-        <button>%</button>
-        <button className="operators">/</button>
-        <button className="gray">7</button>
-        <button className="gray">8</button>
-        <button className="gray">9</button>
-        <button className="operators">X</button>
-        <button className="gray">4</button>
-        <button className="gray">5</button>
-        <button className="gray">6</button>
-        <button className="operators">-</button>
-        <button className="gray">1</button>
-        <button className="gray">2</button>
-        <button className="gray">3</button>
-        <button className="operators">+</button>
-        <button className="gray double">0</button>
-        <button className="gray">,</button>
-        <button>=</button>
+        <button onClick={clearMemory}>AC</button>
+        <button onClick={changeSign}>+/-</button>
+        <button onClick={percentage}>%</button>
+        <button className="operators" onClick={operatorHandler} value={'/'}>
+          /
+        </button>
+        <button className="gray" value={7} onClick={addDigit}>
+          7
+        </button>
+        <button className="gray" value={8} onClick={addDigit}>
+          8
+        </button>
+        <button className="gray" value={9} onClick={addDigit}>
+          9
+        </button>
+        <button className="operators" onClick={operatorHandler} value={'X'}>
+          x
+        </button>
+        <button className="gray" value={4} onClick={addDigit}>
+          4
+        </button>
+        <button className="gray" value={5} onClick={addDigit}>
+          5
+        </button>
+        <button className="gray" value={6} onClick={addDigit}>
+          6
+        </button>
+        <button className="operators" onClick={operatorHandler} value={'-'}>
+          -
+        </button>
+        <button className="gray" value={1} onClick={addDigit}>
+          1
+        </button>
+        <button className="gray" value={2} onClick={addDigit}>
+          2
+        </button>
+        <button className="gray" value={3} onClick={addDigit}>
+          3
+        </button>
+        <button className="operators" onClick={operatorHandler} value={'+'}>
+          +
+        </button>
+        <button className="gray double" value={0} onClick={addDigit}>
+          0
+        </button>
+        <button className="gray" value={','} onClick={addDigit}>
+          ,
+        </button>
+        <button className="operators" onClick={showResult}>
+          =
+        </button>
       </div>
     </div>
   )
